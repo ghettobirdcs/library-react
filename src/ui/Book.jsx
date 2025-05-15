67f45157c584865e7f82c18b53db55c74edab313
McKade Hinshaw <hinshaw.mckade@gmail.com>
2025-05-15 11:27:32 -0600

    Imported useRef

--- src/ui/Book.jsx
+++ src/ui/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating.jsx";
 import Price from "./Price.jsx";
